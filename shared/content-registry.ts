77f6ee5205cfdd6477a78267ce9d37d40588a5a3
Hyldraw <112813424+Hyldraw@users.noreply.github.com>
2025-12-14 01:21:22 +0000

    Atualizar o content-registry.ts

--- shared/content-registry.ts
+++ shared/content-registry.ts
@@ -15,23 +15,23 @@ export interface RegisteredContent {
 }
 
 export const REGISTERED_CONTENT: RegisteredContent[] = [
-  {
+  { // Nos seus sonhos
     tmdbId: "1117857",
     type: "movie",
     playbackUrl: "https://vod-limit-media.123flmsfree.com/60e86579c13371f0800d5114c1ca0102/b02d2ba88f6ab009beca46af2ab4a893-ld.m3u8?hdnts=exp=1766264117_acl=/60e86579c13371f0800d5114c1ca0102/*_hmac=6f755fd1bd108f434f0626c0772af153e25556bc2ecac28b42ab2b0f364dc0d3",
-    status: ['popular', 'featured', 'classic']
+    status: ['featured']
   },
 { // Minecraft: The Movie
     tmdbId: "950387",
     type: "movie",
     playbackUrl: "https://data-limit-c.solo-latino.com/f07b88c55e3171f0bf9f4177d0d10102/ec0843b0bc020c8990fe727507559b16-sd.m3u8?hdnts=exp=1766259869_acl=/f07b88c55e3171f0bf9f4177d0d10102/*_hmac=041991b17e8e4d6b7f490b11032cc9f3e42522ca1eb9c1a50257d8b96aa51398",
-    status: ['popular']
+    status: []
   },
 { // Five nights at Freddy's
     tmdbId: "507089",
     type: "movie",
     playbackUrl: "https://vod-limit-media.123flmsfree.com/00fe1a795e1d71f0bfef87c7371d0102/b0b29cfeaa0b4d959982a1029d173e50-d211a3a7d932b6d147da3e39da3ebb9f-sd.m3u8?hdnts=exp=1766255014_acl=/00fe1a795e1d71f0bfef87c7371d0102/*_hmac=51bc82fd988947a82c557ceb228ad6a8f735bf1d1b06dd90b83a9d6f675db3c3",
-    status: ['popular']
+    status: []
   },
 { // Severance
     tmdbId: "95396",
@@ -57,7 +57,7 @@ export const REGISTERED_CONTENT: RegisteredContent[] = [
 { season: 2, episode: 9, url: "https://data-limit-c.solo-latino.com/b0bb530c106971f090174177d1c00102/ff9874c4b18e4e5390720d6239e8400d-0c70fa2db2fcadf852c93cc505e091f1-sd.m3u8?hdnts=exp=1766272255_acl=/b0bb530c106971f090174177d1c00102/*_hmac=852cbcb050882fe61f4855e48aa8ecec16c4f30afa83ddaed57803cd81bb9a5e" },
 { season: 2, episode: 10, url: "https://data-limit-c.solo-latino.com/f00e5d10106971f0b5195114c1ca0102/5b4d8437514a48aebb4f5b791297c034-12b28bef326dfeef4059329edc7ae5bc-sd.m3u8?hdnts=exp=1766272292_acl=/f00e5d10106971f0b5195114c1ca0102/*_hmac=11ed29846debaac9f4310c9cfa13ab85a665058b93c675b9d000a219fa921cbd" },
     ],
-    status: ['popular']
+    status: []
   }, 
 { // Dark Matter 
     tmdbId: "196322",
@@ -73,9 +73,9 @@ export const REGISTERED_CONTENT: RegisteredContent[] = [
 { season: 1, episode: 8, url: "https://vod-limit.cuevanalatino.net/c08faa701ce571f0907c5114c1ca0102/50ace15820b64ba48daee6e093e41c82-e3e584c4c64bfd86dc447fb5ba743241-sd.m3u8?hdnts=exp=1766264924_acl=/c08faa701ce571f0907c5114c1ca0102/*_hmac=fdd9ff38eecdfeb2d5a44ec985a68e3815fdf169891251fbd9f07b47b69a210f" },
 { season: 1, episode: 9, url: "https://vod-limit.cuevanalatino.net/20a063741ce571f0bff14177d1c10102/c371d9dd59204e31831cd2e3a3743062-07ddbc3eafc6c8649e37f41fada906ff-sd.m3u8?hdnts=exp=1766264965_acl=/20a063741ce571f0bff14177d1c10102/*_hmac=1d6f60911d91282f5f425036bc9e81b3cd871cdd76082cce49a7a4e29a01de02" },
     ],
-    status: ['popular']
+    status: []
   },
-  {
+  { // Breaking Bad
     tmdbId: "1396",
     type: "series",
     episodes: [
@@ -83,67 +83,61 @@ export const REGISTERED_CONTENT: RegisteredContent[] = [
       { season: 1, episode: 2, url: "https://example.com/breaking-bad-s01e02.mp4" },
       { season: 1, episode: 3, url: "https://example.com/breaking-bad-s01e03.mp4" },
     ],
-    status: ['featured', 'popular', 'classic']
+    status: []
   },
-  {
+  { // Stranger Things
     tmdbId: "66732",
     type: "series",
     episodes: [
       { season: 1, episode: 1, url: "https://example.com/stranger-things-s01e01.mp4" },
       { season: 1, episode: 2, url: "https://example.com/stranger-things-s01e02.mp4" },
     ],
-    status: ['featured', 'popular']
+    status: ['featured']
   },
-  {
+  { // The Last of Us
     tmdbId: "100088",
     type: "series",
     episodes: [
       { season: 1, episode: 1, url: "https://example.com/last-of-us-s01e01.mp4" },
       { season: 1, episode: 2, url: "https://example.com/last-of-us-s01e02.mp4" },
     ],
-    status: ['featured', 'trending']
+    status: []
   },
-  {
-  tmdbId: "1084242",      // Pegue no site themoviedb.org
+  { // Zootopia 2
+  tmdbId: "1084242",
   type: "movie",
-  playbackUrl: "https://media-limit-xr8a2.peliculaplay.com/b0eea4cccc2671f09d505114c0db0102/d607a5be5bbc0b427916861014dfe418-ld.m3u8?hdnts=exp=1766184400_acl=/b0eea4cccc2671f09d505114c0db0102/*_hmac=34ea385bd1608e2701e6cfb240b8a2006f72a86d298c2ed25cb23d8d90a22ab5",  // Link do vídeo
-  status: ['featured', 'popular']  // Tags de destaque
+  playbackUrl: "https://media-limit-xr8a2.peliculaplay.com/b0eea4cccc2671f09d505114c0db0102/d607a5be5bbc0b427916861014dfe418-ld.m3u8?hdnts=exp=1766184400_acl=/b0eea4cccc2671f09d505114c0db0102/*_hmac=34ea385bd1608e2701e6cfb240b8a2006f72a86d298c2ed25cb23d8d90a22ab5", 
+  status: ['featured'] 
   },
   { // Oppenheimer
     tmdbId: "872585",
     type: "movie",
     playbackUrl: "https://vod-limit-res.cuevana.life/a01bfb1b259d71f094734177d0d10102/0ad45b8b88d04f2bbff37f81475eb164-dfb32bd8c24ba01cc515bb710b5f3844-ld.m3u8?hdnts=exp=1766181361_acl=/a01bfb1b259d71f094734177d0d10102/*_hmac=75b767e0788e1067a755951f2eed6ba2a79832fec7f2996745207500368b6a82",
-    status: ['featured', 'popular']
+    status: []
   },
-  {
-    tmdbId: "693134",
-    type: "movie",
-    playbackUrl: "https://example.com/dune-part-two.mp4",
-    status: ['featured', 'new']
-  },
-  {
+  { // Barbie Live Action
     tmdbId: "346698",
     type: "movie",
     playbackUrl: "https://example.com/barbie.mp4",
-    status: ['popular']
+    status: []
   },
-{
+{ // Operação Big Hero
     tmdbId: "177572",
     type: "movie",
     playbackUrl: "https://data-limit-c.solo-latino.com/d03355eb660371f088324177d0d10102/396c3321846240da8f54e7df5abcf0ae-56c2966cc5f3cce87b877bc96d2117dc-ld.m3u8?hdnts=exp=1766245428_acl=/d03355eb660371f088324177d0d10102/*_hmac=b8db56ad3b1637d466677e367c7750d2af196e0082e9d0f11ad257b98a0a72d8",
-    status: ['popular']
+    status: []
   },
-  {
+  { // Dune
     tmdbId: "438631",
     type: "movie",
     playbackUrl: "https://example.com/dune.mp4",
-    status: ['popular', 'classic']
+    status: []
   },
-  {
+  { // Interstellar
     tmdbId: "157336",
     type: "movie",
     playbackUrl: "https://example.com/interstellar.mp4",
-    status: ['popular', 'classic']
+    status: []
   }
 ];
 
